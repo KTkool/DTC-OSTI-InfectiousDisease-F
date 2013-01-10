bfbae8a1b944fb9642510254140a82bfae76c726
Kathryn Atwell <mail@kathryn.at>
2013-01-10 16:08:46 +0000

    New paper! Added the Target Cell Limited model.

---
+++ paramsTCL.m
@@ -0,0 +1,17 @@
+%influx rate of target cells
+param.s = 100;
+
+%target cell loss rate
+param.d = 0.01;
+
+%target cell infection rate constant
+param.beta = 1.3e-6;
+
+%infected cell loss rate
+param.delta = 0.5;
+
+%virus production rate
+param.p = 1000;
+
+%virus clearance rate 
+param.c = 3;
