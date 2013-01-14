187b580c1e8d9ec117d3a5b7c11ef1515869ed91
KathrynA <mail@kathryn.at>
2013-01-14 16:58:29 +0000

    Added licensing agreement

--- SolveAndPlotProteasetreatment_CD4D.m
+++ SolveAndPlotProteasetreatment_CD4D.m
@@ -1,3 +1,22 @@
+%{
+Copyright 2013 Ronja Woloszczuk
+
+This file is part of DTC-OSTI-InfectiousDisease-F.
+
+DTC-OSTI-InfectiousDisease-F is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DTC-OSTI-InfectiousDisease-F is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DTC-OSTI-InfectiousDisease-F. If not, see <http://www.gnu.org/licenses/>.
+%}
+
 function [ y ] = SolveAndPlotProteasetreatment_CD4D()
 % Solves ODEs of models one and two using ODE45
 %   
