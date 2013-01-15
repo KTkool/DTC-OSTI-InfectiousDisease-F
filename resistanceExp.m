d05191914fe80650e8ef864d4011934533c455bd
Annekathrin Meiburg <meiburg@DTC127.(none)>
2013-01-15 17:42:28 +0000

    drug efficiency function added

--- resistanceExp.m
+++ resistanceExp.m
@@ -1,7 +1,16 @@
 
 function fval = resistanceExp( t, y, param, k )
-% gives the resistance of the drug
+% gives the effectiveness of the drug
 
-fval = k;
+%fval = k;
+% logistic for RTT---Sorry same function for both is not possible
+fval = param.RTT - k*y(4)*(1-y(4)/param.RTT);
+% for PI it would be:
+% fval = param.PI - k*y(5)*(1-y(5)/param.PI);
+
+% logarithmic
+fval = param.RTT + k*y(4)*ln(y(4)/param.RTT);
+% for PI it would be:
+% fval = param.PI + k*y(5)*ln(y(5)/param.PI);
 
 end
